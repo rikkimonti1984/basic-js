9d45fcab7d470cb490cf6a108233ae863e2abd4b
Rikki Monti <rikkimonti@gmail.com>
2025-01-06 23:01:00 +0500

    feat: add solution getDNSStats function

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsStats = {};
+
+  domains.forEach((domain) => {
+    const parts = domain.split('.').reverse();
+    let currentDomain = '';
+    parts.forEach((part) => {
+      currentDomain += `.${part}`;
+      dnsStats[currentDomain] = (dnsStats[currentDomain] || 0) + 1;
+    });
+  });
+  return dnsStats;
 }
 
 module.exports = {

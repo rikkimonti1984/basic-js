95e8e291f9256e73025117886c330239406346b7
Rikki Monti <rikkimonti@gmail.com>
2025-01-06 23:57:29 +0500

    feat: add solution getEmailDomain-sortByHeight-getSumOfDigits-getSeason function

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const sortedHeights = arr.filter((num) => num !== -1).sort((a, b) => a - b);
+  let index = 0;
+  return arr.map((num) => {
+    if (num === -1) {
+      return -1;
+    } else {
+      return sortedHeights[index++];
+    }
+  });
 }
 
 module.exports = {
